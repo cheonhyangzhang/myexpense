4f54505734e7e429bf2a27e29f6962c470e23a32
Tianxing Zhang <cenhiangapply@gmail.com>
2015-10-20 13:25:43 -0500

    Set up folder structure and basic architecture.

---
+++ config.js
@@ -0,0 +1,10 @@
+var local_db = 'mongodb://localhost:27017/test';
+// var test_db = 'mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o';
+// var dev_db = 'mongodb://45.55.234.147:27017/test';
+
+// var connect_db = test_db;
+var connect_db = local_db;
+module.exports = {
+    'secret': 'ilovescotchyscotch',
+    'database': connect_db 
+};
